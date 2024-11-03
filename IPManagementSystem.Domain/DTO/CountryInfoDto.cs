b46d2e983ff322eaea41fd6d20ff7e1cee3a9598
Gabriel <65770195+GabrielMeneze@users.noreply.github.com>
2024-11-03 20:05:15 -0300

    Apply .gitignore to remove unwanted files

---
+++ IPManagementSystem.Domain/DTO/CountryInfoDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPManagementSystem.Domain.DTO
+{
+    public class CountryInfoDto
+    {
+        public string? CountryName { get; set; }
+        public string? TwoLetterCode { get; set; }
+        public string? ThreeLetterCode { get; set; }
+    }
+}
---
+++ IPManagementSystem.Domain/Entities/IpAddressEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPManagementSystem.Domain.Entities
+{
+    public class IpAddressEntity
+    {
+        public int Id { get; set; }
+        public int CountryId { get; set; }
+        public string? IP { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public Country? Country { get; set; }
+    }
+}
+
